7f677ab2fc2113870dd1272155728196f59527dc
Joseph Levy <josephlevy222@gmail.com>
2023-05-07 13:55:04 -0700

    Improved tests and small size arrays

--- Sources/TridiagonalMatrix/TridiagonalMatrix.swift
+++ Sources/TridiagonalMatrix/TridiagonalMatrix.swift
@@ -7,7 +7,9 @@
 import Foundation
 
 import Numerics
-struct TridiagonalMatrix<T: AlgebraicField > {
+typealias RealOrComplex = AlgebraicField
+
+struct TridiagonalMatrix<T: RealOrComplex > {
     var lower: [T]
     var diagonal: [T]
     var upper: [T]
@@ -25,42 +27,49 @@ struct TridiagonalMatrix<T: AlgebraicField > {
 
 typealias ColumnVector<T> = Array<T>
 
-func *<T: AlgebraicField>(_ A: TridiagonalMatrix<T>, _ x: ColumnVector<T>) -> ColumnVector<T> {
+func *<T: RealOrComplex>(_ A: TridiagonalMatrix<T>, _ x: ColumnVector<T>) -> ColumnVector<T> {
     precondition(x.count == A.size, "Invalid column vector size")
-    var b : ColumnVector<T> = x
     let n = x.count
+    var b : ColumnVector<T> = x
+    if n==0 { return [] }
     b[0] = A.diagonal[0]*x[0]+A.upper[0]*x[1]
+    if n==1 { return b }
+    b[n-1] = A.lower[n-2]*x[n-2]+A.diagonal[n-1]*x[n-1]
+    if n==2 { return b }
     for j in 1..<n-1 {
         b[j] = A.lower[j-1]*x[j-1] + A.diagonal[j]*x[j] + A.upper[j]*x[j+1]
     }
-    b[n-1] = A.lower[n-2]*x[n-2]+A.diagonal[n-1]*x[n-1]
     return b
 }
 /// implements Ax+y where A is a TridiagonalMatrix and x and y are ColumnVector types
-func aXpY<T: AlgebraicField>(A: TridiagonalMatrix<T>, x: ColumnVector<T>, y: ColumnVector<T>) -> ColumnVector<T> {
+func aXpY<T: RealOrComplex>(A: TridiagonalMatrix<T>, x: ColumnVector<T>, y: ColumnVector<T>) -> ColumnVector<T> {
     precondition(x.count == A.size, "Invalid x vector size")
     precondition(y.count == A.size, "Invalid y vector size")
-    var b : ColumnVector<T> = y
     let n = x.count
+    if n==0 { return [] }
+    var b : ColumnVector<T> = y
     b[0] += A.diagonal[0]*x[0]+A.upper[0]*x[1]
+    if n==1 { return b }
+    b[n-1] += A.lower[n-2]*x[n-2]+A.diagonal[n-1]*x[n-1]
+    if n==2 { return b }
     for j in 1..<n-1 {
         b[j] += A.lower[j-1]*x[j-1] + A.diagonal[j]*x[j] + A.upper[j]*x[j+1]
     }
-    b[n-1] += A.lower[n-2]*x[n-2]+A.diagonal[n-1]*x[n-1]
     return b
 }
 
-struct TridiagonalLUMatrix<T: AlgebraicField> {
-    private var au0 : [T]
-    private var au1 : [T]
-    private var au2 : [T]
-    private var al  : [T]
-    private var indx : [Int]
-    private var d : Bool
-    var singular : Bool
-    var smallestPivot : T.Magnitude
+struct TridiagonalLUMatrix<T: RealOrComplex > {
+    private var au0 = [T]()
+    private var au1 = [T]()
+    private var au2 = [T]()
+    private var al  = [T]()
+    private var indx = [Int]()
+    private var d : Bool = true
+    var singular : Bool = false
+    var approximateConditionNumber : T.Magnitude?
     init(_ A: TridiagonalMatrix<T>) {
         let n = A.size
+        if n==0 { return }
         al = Array(repeating: T.zero, count: n)
         indx = Array(0..<n)
         d = true
@@ -70,38 +79,41 @@ struct TridiagonalLUMatrix<T: AlgebraicField> {
         au2 = A.upper + [0]
         au1[0] = au2[0]
         au2[0] = T.zero // completes rearrangement
-        smallestPivot = au0[0].magnitude
+        let maxElement = (au0+au1+au2).map {$0.magnitude}.max()
+        approximateConditionNumber = au0[0].reciprocal?.magnitude // nil if infinite
         for k in 0..<n {
             // Find the biggest pivot and check if 0
             var au0max = au0[k].magnitude
             var i = k
-            if k<n-1 { //for j in k+1..<el { // j = k+1 or none
+            if k<n-1 {
                 let au0p1mag = au0[k+1].magnitude
                 if au0p1mag > au0max {
                     au0max = au0p1mag
                     i = k+1
                 }
             }
-            if smallestPivot > au0max { smallestPivot = au0max }
+            if let condition = approximateConditionNumber, let a =  au0[k].reciprocal?.magnitude, condition<a { approximateConditionNumber = a }
             indx[k] = i+1
-            if au0max == 0 { singular = true; print("singular") }  // want au0[k]=tinyValue
+            if au0max == 0 { singular = true; print("singular") }  
             if i != k {
                 d.toggle() // to get sign of determinate
                 au0.swapAt(k, i)
                 au1.swapAt(k, i)
                 au2.swapAt(k, i)
             }
-            if k<n-1 { //for i in k+1..<el { // i is k+1 el is k+1 or k if k+1 is n-1
+            if k<n-1 {
                 al[k] = au0[k+1]/au0[k] // fix for singular (maybe?)
                 au0[k+1] = au1[k+1] - al[k] * au1[k]
                 au1[k+1] = au2[k+1] - al[k] * au2[k]
                 au2[k+1] = T.zero
             }
         }
+        if let condition = approximateConditionNumber, let m =  maxElement { approximateConditionNumber = condition*m }
     }
     
     func solveInPlace(_ x: inout ColumnVector<T>) {
         let n = au0.count
+        if n==0 {return}
         for k in 0..<n {
             x.swapAt(k, indx[k]-1)
             if k<n-1 { x[k+1] -= al[k]*x[k] }
--- Tests/TridiagonalMatrixTests/TridiagonalMatrixTests.swift
+++ Tests/TridiagonalMatrixTests/TridiagonalMatrixTests.swift
@@ -1,74 +1,81 @@
 import XCTest
 @testable import TridiagonalMatrix
 import Numerics
+func roundoff<T: RealOrComplex>(_ d: T) -> T.Magnitude {
+    if d.magnitude is Double { return Double.ulpOfOne*2 as? T.Magnitude ?? .zero }
+    if d.magnitude is Float { return Float.ulpOfOne*2 as? T.Magnitude ?? .zero }
+    if d.magnitude is Float80 { return Float80.ulpOfOne*2 as? T.Magnitude ?? .zero}
+    return Double.ulpOfOne*2 as? T.Magnitude ?? .zero
+}
+
+func aboutEquals<T: RealOrComplex>(_ d1: T, _ d2: T) -> Bool {
+    if d1 is Complex<Double> { return (d1 as! Complex<Double>).isApproximatelyEqual(to: d2 as! Complex<Double>) }
+    if d1 is Complex<Float> { return (d1 as! Complex<Float>).isApproximatelyEqual(to: d2 as! Complex<Float>)}
+    if d1 is Complex<Float80> { return(d1 as! Complex<Float80>).isApproximatelyEqual(to: d2 as! Complex<Float80>)}
+    if d1 is Float { return (d1 as! Float).isApproximatelyEqual(to: d2 as! Float)}
+    if d1 is Double { return (d1 as! Double).isApproximatelyEqual(to: d2 as! Double)}
+    if d1 is Float80 { return (d1 as! Float80).isApproximatelyEqual(to: d2 as! Float80)}
+    return true
+}
+
 final class TridiagonalMatrixTests: XCTestCase {
     func testNormal() throws {
-        try testExample(Complex<Double>(2.0,0.0))
+        try testExample(Complex<Double>(2.0,0.0), det: Complex(6.0))
     }
     func testSingular() throws {
-        try testExample(Complex<Double>(1.0,0.0))
+        try testExample(Complex<Float>(1.0,0.0), det: 0)
     }
     func testNearlySingular() throws {
-        try testExample(Complex<Double>(1.732051,0.0))
+        try testExample(Complex<Float>(1.732051,0.0), det: Complex(2.265e-6))
     }
     
-    func testExample(_ d: Complex<Double> ) throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-     
-        let one = Complex<Double>(1.0,0.0)
-        //let d = Complex<Double>(1.0,0.0) // nearly singular
+    func testExample<T : RealOrComplex >(_ d: T, det: T) throws {
+        guard let one = T(exactly: 1) else { print("Test of type like \(d) not possible"); return }
         let lower = [-one,-one,-one,-one]
         let upper = lower
         let diagonal = [d,d,d,d,d]
         let tridiag = TridiagonalMatrix(diagonal: diagonal, upper: upper, lower: lower)
         let tridiagLU = TridiagonalLUMatrix(tridiag)
-        let i0 = [one,0,0,0,0]
-        let i1 = [0,one,0,0,0]
-        let i2 = [0,0,one,0,0]
-        let i3 = [0,0,0,one,0]
-        let i4 = [0,0,0,0,one]
-        let x0 = tridiagLU.solve(i0)
-        let x1 = tridiagLU.solve(i1)
-        let x2 = tridiagLU.solve(i2)
-        let x3 = tridiagLU.solve(i3)
-        let x4 = tridiagLU.solve(i4)
-        //print(tridiag)
-        let ii0 = tridiag * x0; //print(ii0)
-        let ii1 = tridiag * x1; //print(ii1)
-        let ii2 = tridiag * x2; //print(ii2)
-        let ii3 = tridiag * x3; //print(ii3)
-        let ii4 = tridiag * x4; //print(ii4)
-        let tiny = 1e-15/tridiagLU.smallestPivot
-        print("tiny=\(tiny), smallestPivot=\(tridiagLU.smallestPivot)")
-        let e0 =  zip(i0, ii0).map { $0 - $1 } // error (difference) in identity matrix
-        let e1 =  zip(i1, ii1).map { $0 - $1 }
-        let e2 =  zip(i2, ii2).map { $0 - $1 }
-        let e3 =  zip(i3, ii3).map { $0 - $1 }
-        let e4 =  zip(i4, ii4).map { $0 - $1 }
-        var result = e0.reduce(true) { $0 && $1.magnitude < tiny }
-        result = e1.reduce(result) { $0 && $1.magnitude < tiny }
-        result = e2.reduce(result) { $0 && $1.magnitude < tiny }
-        result = e3.reduce(result) { $0 && $1.magnitude < tiny }
-        result = e4.reduce(result) { $0 && $1.magnitude < tiny }
-        var maxError = e0.reduce(0) { max($0,$1.magnitude) }
-        maxError = e1.reduce(maxError) { max($0,$1.magnitude) }
-        maxError = e2.reduce(maxError) { max($0,$1.magnitude) }
-        maxError = e3.reduce(maxError) { max($0,$1.magnitude) }
-        maxError = e4.reduce(maxError) { max($0,$1.magnitude) }
-        print("maxError=\(maxError)")
-        let det = tridiagLU.determinate(); print("det=\(det)")
-        switch d {
-        case Complex<Double>(2.0,0.0) :
-            XCTAssertEqual(Complex<Double>(6.0, 0.0), det)
-        case  Complex<Double>(1.0,0.0) :
-            XCTAssertEqual(Complex<Double>(0.0, 0.0), det)
-        
-        default :
-            XCTAssertLessThan(.zero, det.magnitude)
+        var i = Array<[T]>(repeating: [], count: 5)
+        i[0] = [one,0,0,0,0]
+        i[1] = [0,one,0,0,0]
+        i[2] = [0,0,one,0,0]
+        i[3] = [0,0,0,one,0]
+        i[4] = [0,0,0,0,one]
+        var x = i
+        for j in 0..<i.count {
+            x[j] = tridiagLU.solve(i[j])
         }
-            
-       XCTAssertTrue(result || tridiagLU.singular)
+        var ii = i
+        for j in 0..<i.count {
+            ii[j] = tridiag*x[j]
+        }
+
+        var tolerance : T.Magnitude = roundoff(d)
+        if let condition = tridiagLU.approximateConditionNumber {
+            print("conditionNumber=\(condition)")
+            tolerance *= condition
+        }
+        print("tolerance=\(tolerance)")
+        var e = i
+        for j in 0..<i.count { // error (difference) in identity matrix
+            e[j] = zip(i[j],ii[j]).map { $0 - $1 }
+        }
+        var result = true
+        for j in 0..<i.count {
+            result = e[j].reduce(result) { $0 && ($1.magnitude) < tolerance }
+        }
+
+        var maxError = T.Magnitude.zero
+        for j in 0..<i.count {
+            maxError = e[j].reduce(maxError) { max($0,$1.magnitude) }
+        }
+        print("maxError=\(maxError)")
+        let determinate = tridiagLU.determinate()
+        print("determinate=\(determinate) vs. \(det)")
+    
+        XCTAssertTrue(aboutEquals(determinate,det))
+        
+        XCTAssertTrue(result || tridiagLU.singular)
     }
 }
